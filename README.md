284e1fc5bc9d6ab8fd0e805cac83534fdc1d1fad
TangQianYong <qy_tang@yeah.net>
2020-04-03 12:41:04 -0400

    更改结构，增加简介和tags

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-python3 -m http.server
+1. 根目录运行: python3 -m http.server
+2. 访问: http://localhost:8000/
--- js/test.js
+++ js/test.js
@@ -4,7 +4,7 @@ function getPeople() {
 	var rowItemNum = 5;
 	$.ajax({
 		type: "get",
-		url: "./json/test.json",
+		url: "../json/test.json",
 		dataType: "json",
 		success: function(res) {
 			var list = res
@@ -16,12 +16,14 @@ function getPeople() {
 					tmp = '<div class="comList" id = "comList"></div>';
 				}
 				tmp +=
-				'<a class="comItem" href=##>' +
+				'<a  class = "comItem" href=##>' +
 				'<div data-attid="' + list[$i].image + '"style="background-image: url(' + list[$i].image + ');"></div>' +
 				'<div class="comItemDivider"></div>' +
 				'<div class="comItemName">'+list[$i].name_zh+'</div>' +
-				'<div class="comItemAbstract">'+list[$i].description+'</div>' +
-				'</a>';
+				'<div class="comItemAbstract">'+'所在地: ' + list[$i].location+'</div>' +
+				'<div class="comItemAbstract">'+'简介: ' + list[$i].description+'</div>' +
+				'<div class="comItemAbstract">'+'标签: ' + list[$i].tags + '</div>' +
+				'</a>'
 			}
 			tmp += '</div>';
 			$("#comList").append(tmp);
