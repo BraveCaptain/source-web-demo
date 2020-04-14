5f89327cab05866ca2579c1238068cbd383c95ec
TangQianYong <qy_tang@yeah.net>
2020-04-14 12:50:18 -0400

    上一页下一页完成

--- static/test.js
+++ static/test.js
@@ -1,40 +1,5 @@
 getPeople();
 
-function query(queryPage) {
-	var queryPage = queryPage||1;
-	console.log("query page = " + queryPage);
-	displayPage('#pageDiv', 'goPage', 'query', 10, 100);
-}
-
-function displayPage(divObj,inputPage,js,totalPages,currentPage,totalRecords) {
-	divObj = divObj||"body";
-    inputPage = inputPage||"goPage";
-    js=js||"query";
-    totalPages=totalPages||1;
-    totalRecords=totalRecords||0;
-    currentPage=currentPage||1;
-    var str = "当前第&ensp;<span name='currentPage'>"+currentPage+"</span>&ensp;页";
-    str += "&ensp;共&ensp;<span name='totalPages'>"+totalPages+"</span>&ensp;页";
-    str += "&ensp;<a href='javascript:void(0)' onclick='"+js+"(1)'>首页</a>";
-    if(currentPage>1) {
-        str += "&ensp;<a href='javascript:void(0)' onclick='"+js+"("+(currentPage-1)+")'>上一页</a>";
-    }
-	else {
-        str += "&ensp;<a>上一页</a>";
-    }
-    if(currentPage<totalPages) {
-        str += "&ensp;<a href='javascript:void(0)' onclick='"+js+"("+(currentPage+1)+")'>下一页</a>";
-    }
-	else {
-        str += "&ensp;<a>下一页</a>";
-    }
-    str += "&ensp;<a href='javascript:void(0)' onclick='"+js+"("+totalPages+")'>尾页</a>";
-    str += "&ensp;跳转到&ensp;<input type='number' min=1 max="+totalPages+" id='"+inputPage+"' value="+currentPage+" style='width:40px'/>";
-    str += "&ensp;<input type='button' value='go' onclick='var num=$(\"#"+inputPage+"\").val();"+js+"(num)'/>";
-    $(divObj).empty();
-    $(divObj).append(str);
-}
-
 function getPeople(peopleSrc) {
 	var tmp = '';
 	var rowItemNum = 5;
