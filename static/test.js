bc2cee8dc0f569b26b8a4a745904b2d7ebc60328
zhaoli <zhaol@suosi.com.cn>
2020-04-15 12:27:33 +0800

    * 优化翻页等功能

--- static/test.js
+++ static/test.js
@@ -1,16 +1,51 @@
-getPeople();
-
-function getPeople(peopleSrc) {
+function getPeople(cat, page_num) {
 	var tmp = '';
 	var rowItemNum = 5;
-	var peopleSrc = peopleSrc||"../json/test.json";
+    var last_page;
+    var next_page;
+
+    // CATEGORY
+    cat_element = document.getElementById('category_name');
+    cat_text_last = cat_element.innerText;
+    console.log(cat_text_last);
+    if (cat != '' && cat != cat_text_last) {
+        cat_element.innerHTML = cat;
+    }
+
+    // PAGE_ID
+    last_element = document.getElementById("page_id");
+    last_page = parseInt(last_element.innerText, 10);
+    next_page = last_page;
+
+    if (page_num == -1) {
+        if (last_page > 1) {
+            next_page = last_page - 1;
+            cat = cat_text_last;
+        }
+    } else if (page_num == -2) {
+        next_page = last_page + 1;
+        cat = cat_text_last;
+    } else {
+        next_page = page_num;
+    }
+
+    if (next_page != last_page) {
+        last_element.innerHTML = next_page.toString();
+    }
+
+    if (cat_text_last == "全部") {
+        cat = '';
+    }
+
 	$.ajax({
 		type: "get",
-		url: peopleSrc,
+		url: "http://192.168.101.193:8001/gallery",
 		dataType: "json",
+        data:{"g_type":"人物", "g_cat": cat, "page_id":next_page},
 		success: function(res) {
-			var list = res.datas
+			var list = res.data
 			console.log(list)
+			$("#comList").empty();
 			for(var $i = 0; $i < list.length; $i++) {
 				if($i > 0 && $i % rowItemNum == 0) {
 					tmp += '</div>';
@@ -18,17 +53,18 @@ function getPeople(peopleSrc) {
 					tmp = '<div class="comList" id = "comList"></div>';
 				}
 				tmp +=
-				'<a  class = "comItem" href=##>' +
-				'<div data-attid="' + list[$i].image + '"style="background-image: url(' + list[$i].image + ');"></div>' +
-				'<div class="comItemDivider"></div>' +
-				'<div class="comItemName">'+list[$i].name_zh+'</div>' +
-				'<div class="comItemAbstract">'+'所在地: ' + list[$i].location+'</div>' +
-				'<div class="comItemAbstract">'+'简介: ' + list[$i].description+'</div>' +
-				'<div class="comItemAbstract">'+'标签: ' + list[$i].tags + '</div>' +
-				'</a>'
+                '<a class="comItem" href=##>' +
+                '<div data-attid="' + list[$i].image_s + '"style="background-image: url(' + 'http://192.168.101.193:8001/image/' + list[$i].image_s + ');"></div>' +
+                '<div class="comItemDivider"></div>' +
+                '<div class="comItemName">'+list[$i].name_zh_s+'</div>' +
+                '<div class="comItemAbstract">'+'<strong>国家:</strong> ' + list[$i].country_s+'</div>' +
+                '<div class="comItemAbstract">'+'<strong>组织:</strong> ' + list[$i].member_s+'</div>' +
+                '<div class="comItemAbstract">'+'<strong>职位:</strong> ' + list[$i].position_s+'</div>' +
+                '<div class="comItemAbstract">'+'<strong>简介:</strong> ' + list[$i].description_zh_seg+'</div>' +
+                '<div class="comItemAbstract">'+'<strong>标签:</strong> ' + list[$i].occupation_s + '</div>' +
+                '</a>';
 			}
 			tmp += '</div>';
-			$("#comList").empty();
 			$("#comList").append(tmp);
 		}
 	});
